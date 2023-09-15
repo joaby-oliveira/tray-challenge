de5d6b755ace35548049c326febb9b25ba2662c3
Joaby <joabyoliveira2004@gmail.com>
2023-09-15 20:15:03 -0300

    feat: implement whatsapp send message

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
-console.log("ASDLKAJSKLDJ")
+import * as dotenv from "dotenv";
+dotenv.config();
+
+async function sendMessage() {
+  console.log(process.env.WHATSAPP_API_URL)
+  console.log(process.env.WHATSAPP_API_TOKEN)
+  const responseRaw = await fetch(process.env.WHATSAPP_API_URL ?? "", {
+    method: "POST",
+    body: JSON.stringify({
+      phone: "+5514981010735",
+      message: "Hello, World!",
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      Token: process.env.WHATSAPP_API_TOKEN ?? "",
+    },
+  });
+
+  const response = await responseRaw.json();
+
+  console.log(response);
+}
+
+sendMessage()
