58210709e4f9a395ebdadda3811092141c49c7aa
Joaby <joabyoliveira2004@gmail.com>
2023-09-15 20:39:08 -0300

    feat: add server json

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,26 @@ import * as dotenv from "dotenv";
 import { Env } from "./utils/Env";
 dotenv.config();
 
-async function sendMessage() {
-  const responseRaw = await fetch(Env.get("WHATSAPP_API_URL"), {
-    method: "POST",
-    body: JSON.stringify({
-      phone: "+5514981010735",
-      message: "Hello, World!",
-    }),
-    headers: {
-      "Content-Type": "application/json",
-      Token: Env.get("WHATSAPP_API_TOKEN"),
-    },
-  });
+// async function sendMessage() {
+//   const responseRaw = await fetch(Env.get("WHATSAPP_API_URL"), {
+//     method: "POST",
+//     body: JSON.stringify({
+//       phone: "+5514981010735",
+//       message: "Hello, World!",
+//     }),
+//     headers: {
+//       "Content-Type": "application/json",
+//       Token: Env.get("WHATSAPP_API_TOKEN"),
+//     },
+//   });
 
-  const response = await responseRaw.json();
+//   const response = await responseRaw.json();
+// }
 
-  console.log(response);
+// sendMessage();
+
+function bootstrap() {
+  
 }
 
-sendMessage();
+bootstrap();
