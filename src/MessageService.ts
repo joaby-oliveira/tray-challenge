c50051dda3ea29552617c1aee98f56e2ba835566
Joaby <joabyoliveira2004@gmail.com>
2023-09-15 20:53:18 -0300

    feat: create entities classes

--- src/MessageService.ts
+++ src/MessageService.ts
@@ -1,3 +1,27 @@
+import { Env } from "./utils/Env";
+
 export class MessageService {
-    
+  private token!: string;
+  private url!: string;
+
+  constructor(token: string, url: string) {
+    this.token = token;
+    this.url = url;
+  }
+  public async sendMessage() {
+    const responseRaw = await fetch(Env.get("WHATSAPP_API_URL"), {
+      method: "POST",
+      body: JSON.stringify({
+        phone: "+5514981010735",
+        message: "Hello, World!",
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Token: Env.get("WHATSAPP_API_TOKEN"),
+      },
+    });
+
+    const response = await responseRaw.json();
+    return response;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,9 @@
 import * as dotenv from "dotenv";
-import { Env } from "./utils/Env";
 dotenv.config();
-
-// async function sendMessage() {
-//   const responseRaw = await fetch(Env.get("WHATSAPP_API_URL"), {
-//     method: "POST",
-//     body: JSON.stringify({
-//       phone: "+5514981010735",
-//       message: "Hello, World!",
-//     }),
-//     headers: {
-//       "Content-Type": "application/json",
-//       Token: Env.get("WHATSAPP_API_TOKEN"),
-//     },
-//   });
-
-//   const response = await responseRaw.json();
-// }
-
-// sendMessage();
+import { scheduleJob } from "node-schedule";
 
 function bootstrap() {
-  
+  scheduleJob("*/5 * * * * *", () => {});
 }
 
 bootstrap();
