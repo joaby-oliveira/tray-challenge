38924cefa9bfd6347a50cb5c6b0f124caa75ea46
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2023-09-15 22:22:23 -0300

    feat: implement

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,23 @@ import { scheduleJob } from "node-schedule";
 import { Factory } from "./utils";
 
 function bootstrap() {
+  
   const mailService = Factory.makeEmailService();
   const cartService = Factory.makeCartService();
-
-  scheduleJob("*/2 * * * *", async () => {
+  scheduleJob("*/10 * * * *", async () => {
     const carts = await cartService.getCarts();
 
     if (carts.length > 0) {
       for (const cart of carts) {
-        mailService.sendEmail({
-          from: "ichacaramarketplace@gmail.com",
-          to: cart.email,
-          subject: "Ei, acho que você esqueceu de alguma coisa!",
-          text: "",
-        });
+        // Send email asynchronously
+        setTimeout(() => {
+          mailService.sendEmail({
+            from: "ichacaramarketplace@gmail.com",
+            to: cart.email,
+            subject: "Ei, acho que você esqueceu de alguma coisa!",
+            text: "",
+          });
+        }, 0);
         const messageService = Factory.makeMessageService();
         const res = await messageService.sendMessage(cart);
         console.log(res);
